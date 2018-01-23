b8cfbeb41ae1bb9c4352cc1a8ea6a3c143a7e6f9
tkazbekov <tkazbekov@gmail.com>
2018-01-24 03:57:57 +0600

    Release 0.0.1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
-import { videojs } from '../../../node_modules/video.js/dist/video.es.js';
-import * as video from 'video.js';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild, NgZone } from '@angular/core';
+import * as videojs from 'video.js';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -11,22 +10,55 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent implements OnInit, AfterViewInit {
 
   @ViewChild('video', {read: ElementRef}) videoref: ElementRef;
+  @ViewChild('video2') canvas: ElementRef;
+  public context: CanvasRenderingContext2D;
   coordinates: any;
 
-  constructor( private http: HttpClient) {
-  }
+  constructor(
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {
-    this.getJson().subscribe( data => this.coordinates = data);
+    this.getJson().subscribe( data => {
+      this.coordinates = data;
+      videojs(this.videoref.nativeElement, {}, function() {});
+    });
   }
 
   ngAfterViewInit() {
-    video(this.videoref.nativeElement, {}, function() {
-
+    this.context = (<HTMLCanvasElement>this.canvas.nativeElement).getContext('2d');
+    this.context.fillStyle = '#000000';
+    this.videoref.nativeElement.addEventListener('timeupdate', () => {
+    this.context.clearRect(0, 0, 1000, 562);
+      this.context.drawImage(this.videoref.nativeElement, 0, 0, 1000, 562);
+      const currentFrame = Math.abs(this.videoref.nativeElement.currentTime * 24);
+      for (const key in this.coordinates) {
+        if (currentFrame < parseInt(key, 10)) {
+          this.drawBanners(this.coordinates[key]);
+          break;
+        }
+      }
     });
   }
 
   getJson() {
     return this.http.get('/../assets/video/cartesian.json');
   }
+
+  drawBanners(data) {
+    this.context.moveTo(0, 0);
+    this.context.beginPath();
+    for (let i = 0; i < data.length; i++) {
+      const x = data[i]['x'];
+      const y = data[i]['y'];
+      const w = data[i]['w'];
+      const h = data[i]['h'];
+      this.context.rect(x, y, w, h);
+    }
+    this.context.fill();
+  }
+  step() {
+    console.log('Step!');
+  }
+
 }
