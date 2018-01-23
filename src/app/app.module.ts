3db61e3f1ae97a0aa0480adbeb648d9106b56586
Timur Kazbekov <TKazbekov@dar.lcl>
2018-01-23 22:44:37 +0600

    feat(home): added video player and coordinates hook

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
@@ -11,6 +11,7 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     CoreModule
   ],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { videojs } from '../../../node_modules/video.js/dist/video.es.js';
+import * as video from 'video.js';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('video', {read: ElementRef}) videoref: ElementRef;
+  coordinates: any;
 
-  ngOnInit() {
+  constructor( private http: HttpClient) {
   }
 
+  ngOnInit() {
+    this.getJson().subscribe( data => this.coordinates = data);
+  }
+
+  ngAfterViewInit() {
+    video(this.videoref.nativeElement, {}, function() {
+
+    });
+  }
+
+  getJson() {
+    return this.http.get('/../assets/video/cartesian.json');
+  }
 }
